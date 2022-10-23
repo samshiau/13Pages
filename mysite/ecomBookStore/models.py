0bac5766843d0a07ad888711c1e7f62bf58d0d6c
UniverseJason <011203jasonlee@gmail.com>
2022-10-22 22:21:13 -0500

    update admin and register

--- mysite/ecomBookStore/models.py
+++ mysite/ecomBookStore/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model): 
+    item_id = models.AutoField(primary_key=True)
+    item_name = models.CharField(max_length=100)
+    item_type = models.CharField(max_length=100)
+    item_price = models.IntegerField()
+    item_quantity = models.IntegerField()
--- mysite/ecomBookStore/views.py
+++ mysite/ecomBookStore/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+from .models import Item
 
 
 def index(request):
     return render(request, 'index.html')
 
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+    return render(request, 'register.html')

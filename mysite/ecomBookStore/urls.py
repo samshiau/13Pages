db1b92ad267ee8d6df198f54db8bd3ffab3ca131
UniverseJason <011203jasonlee@gmail.com>
2022-11-10 01:10:06 -0600

    admin updated

--- mysite/ecomBookStore/urls.py
+++ mysite/ecomBookStore/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('register', views.register, name='register'),
     path('login', views.login, name='login'),
     path('cart', views.cart, name='cart'),
-    path('admin/', admin.site.urls),
+    path('admin', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) # image config

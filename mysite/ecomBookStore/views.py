52ea8f35250c0af08a28db0b96dfa538fa5b7612
DEVO27 <A.G-Zoe@outlook.com>
2022-11-17 00:02:24 -0600

    Fixed bug in Login Page

--- mysite/ecomBookStore/views.py
+++ mysite/ecomBookStore/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth.models import User, auth
 from django.contrib import messages
+from django.shortcuts import render, redirect
+
 from .models import *
+
 
 def index(request):
     products = product.objects.all()
@@ -14,7 +15,7 @@ def index(request):
         tagFilter = request.GET.get('item_filter')
 
         try:
-            product_filter_tag = filter_tag.objects.get(tagName = tagFilter)
+            product_filter_tag = filter_tag.objects.get(tagName=tagFilter)
             products = product_filter_tag.products.all()
         except:
             print("Error: No filter tag found")
@@ -22,8 +23,22 @@ def index(request):
 
     return render(request, 'index.html', {'products': products, 'filter_tags': filter_tags})
 
+
 def login(request):
+    if request.method == 'POST':
+        if request.POST['email'] and request.POST['password'] != None:
+            email = str(request.POST['email']).strip().lower()
+            password = request.POST['password']
+
+            if User.objects.filter(email=email).exists():
+                print(User.objects.filter(password=password).exists())
+                return redirect('index')
+
+            messages.info(request, 'Account does not exists')
+            return redirect('register')
+
     return render(request, 'login.html')
+
 
 def cart(request):
     return render(request, 'cart.html')
@@ -31,10 +46,10 @@ def cart(request):
 
 def register(request):
     if request.method == 'POST':
-        username = request.POST['username']
-        email = request.POST['email']
-        password = request.POST['password']
-        confirm_password = request.POST['confirm_password']
+        username = str(request.POST['username']).strip()
+        email = str(request.POST['email']).strip().lower()
+        password = str(request.POST['password']).strip()
+        confirm_password = str(request.POST['confirm_password']).strip()
 
         # check if the passwords match
         if password == confirm_password:
@@ -44,14 +59,13 @@ def register(request):
             elif User.objects.filter(username=username).exists():
                 messages.info(request, 'Username already exists')
                 return redirect('register')
-            else: # new user
+            else:  # new user
                 user = User.objects.create_user(username=username, email=email, password=password)
                 user.save()
                 return redirect('index')
-        else: # incorrect password
+        else:  # incorrect password
             messages.info(request, 'Passwords do not match')
             return redirect('register')
-    
-    else: # GET request
+
+    else:  # GET request
         return render(request, 'register.html')
-    
